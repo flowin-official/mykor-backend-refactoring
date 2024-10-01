e4034f6116194f9732557c30bccffe9a40fb9647
hyukkyo <shk5644@gmail.com>
2024-10-01 23:43:35 +0900

    gitignore 수정2

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../mykor-430210-firebase-adminsdk-u16ux-1622a15330.json");
+const serviceAccount = require("../mykor-430210-firebase-adminsdk-u16ux-5202f74f52.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
