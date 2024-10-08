4dab9d0f9531093d415fe6d8a4a256bc3bb007ce
hyukkyo <shk5644@gmail.com>
2024-10-08 18:33:56 +0900

    fix: presigned url 가져오기

--- src/controllers/s3Controller.js
+++ src/controllers/s3Controller.js
@@ -61,9 +61,9 @@ async function postPresignedUrl(req, res) {
     const { purpose, count } = req.body;
 
     // presigned URL 생성
-    const { urls } = await generatePresignedUrl(userId, purpose, count);
+    const urls = await generatePresignedUrl(userId, purpose, count);
 
-    return res.status(200).json({ urls });
+    return res.status(200).json(urls);
   } catch (error) {
     return res.status(500).json({ error: "Failed to generate presigned URL" });
   }
