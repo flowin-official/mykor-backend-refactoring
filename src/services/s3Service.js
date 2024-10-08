d5dfd9fa9f52ae7bee77ea7ba18f81940d6ba87a
hyukkyo <shk5644@gmail.com>
2024-10-08 17:14:17 +0900

    fix: presigned-url 수정

--- src/services/s3Service.js
+++ src/services/s3Service.js
@@ -6,9 +6,6 @@ const generatePresignedUrl = (userId, purpose, count) => {
   if (purpose === "profile") {
     key = `profile/${userId}`;
   } else if (purpose === "post") {
-    for (let i = 0; i < count; i++) {
-      key = `post/${userId}/${Date.now()}_${i}`;
-    }
   } else {
     throw new Error("Invalid purpose");
   }
@@ -19,8 +16,7 @@ const generatePresignedUrl = (userId, purpose, count) => {
     Expires: 60, // Presigned URL의 유효 기간 (기본: 60초)
   };
 
-  const url = s3.getSignedUrlPromise("putObject", params); // 쓰기용 presigned URL
-  return { url, key }; // 쓰기용 presigned URL
+  return s3.getSignedUrlPromise("putObject", params); // 쓰기용 presigned URL
 };
 
 module.exports = { generatePresignedUrl };
