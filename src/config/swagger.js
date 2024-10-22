753a28bd021915b89c26f4884dfa665f5d30b1db
hyukkyo <shk5644@gmail.com>
2024-10-22 18:25:59 +0900

    fix: 스웨거 폐쇄 재재시도

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,32 +1,32 @@
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const { SWAGGER_ADDRESS, SERVER_PORT } = process.env;
+// const { SWAGGER_ADDRESS, SERVER_PORT } = process.env;
 
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
+// const swaggerJsDoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
 
-const swaggerDefinition = {
-  openapi: "3.0.0",
-  info: {
-    title: "MyKor API",
-    version: "1.0.0",
-    description: "API documentation for the MyKor project",
-  },
-  servers: [
-    {
-      url: `http://${SWAGGER_ADDRESS}:${SERVER_PORT}/mykor/api/v1`,
-    },
-  ],
-};
+// const swaggerDefinition = {
+//   openapi: "3.0.0",
+//   info: {
+//     title: "MyKor API",
+//     version: "1.0.0",
+//     description: "API documentation for the MyKor project",
+//   },
+//   servers: [
+//     {
+//       url: `http://${SWAGGER_ADDRESS}:${SERVER_PORT}/mykor/api/v1`,
+//     },
+//   ],
+// };
 
-const options = {
-  swaggerDefinition,
-  apis: ["./src/controllers/*.js"],
-};
+// const options = {
+//   swaggerDefinition,
+//   apis: ["./src/controllers/*.js"],
+// };
 
-const swaggerSpec = swaggerJsDoc(options);
+// const swaggerSpec = swaggerJsDoc(options);
 
-module.exports = (app) => {
-  // app.use("/mykor/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-};
+// module.exports = (app) => {
+//   app.use("/mykor/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// };
