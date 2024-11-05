967748d422ce86e88cc1e86fe7a250ba62c27a42
hyukkyo <shk5644@gmail.com>
2024-11-05 17:36:37 +0900

    fix: fct test

--- src/utils/fct.js
+++ src/utils/fct.js
@@ -1,12 +1,7 @@
 const admin = require("../config/firebase");
 
 async function generateFirebaseCustomToken(userId) {
-  try {
-    const customToken = await admin.getAuth().createCustomToken(userId);
-    return customToken;
-  } catch (error) {
-    throw error;
-  }
+  return await admin.auth().createCustomToken(userId);
 }
 
 module.exports = {
